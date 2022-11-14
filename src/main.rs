6e2cab527f2b8a3ba9b92cebd62876e79439377b
Brenda Streiff <git@streiff.net>
2022-11-13 21:35:23 -0600

    add charting

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,21 @@ extern crate is_sorted;
 use is_sorted::IsSorted;
 
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
+use std::ops::Range;
+use std::process::Command;
 
 struct ColorName {
     name: String,
     abbr: String,
+}
+
+struct ColorBlock {
+    color_id: u32,
+    hues: Range<usize>,
+    chromas: Range<usize>,
+    values: Range<usize>,
 }
 
 fn add_name_to_map(map: &mut HashMap<u32, ColorName>, node: roxmltree::Node) {
@@ -76,7 +87,7 @@ fn validate_name_map(map: &HashMap<u32, ColorName>) {
     }
 }
 
-fn validate_names(doc: &roxmltree::Document) {
+fn validate_names(doc: &roxmltree::Document) -> HashMap<u32, ColorName> {
     let names = doc.descendants().find(|n| n.has_tag_name("names")).unwrap();
 
     let mut level1_names = HashMap::new();
@@ -96,6 +107,8 @@ fn validate_names(doc: &roxmltree::Document) {
     validate_name_map(&level1_names);
     validate_name_map(&level2_names);
     validate_name_map(&level3_names);
+
+    return level3_names;
 }
 
 fn get_hues(doc: &roxmltree::Document) -> Vec<String> {
@@ -151,13 +164,15 @@ fn validate_blocks(
     hues: &Vec<String>,
     chromas: &Vec<String>,
     values: &Vec<String>,
-) {
+) -> Vec<ColorBlock> {
     // The lookup table is logically a three-dimensional array, but initializing a
     // vector of vectors of vectors is Actually Kind Of A Pain?
     //
     // We remove one from chroma and values length because of the INF at the end.
     let mut lookup_table: Vec<u32> =
         Vec::with_capacity(hues.len() * (chromas.len() - 1) * (values.len() - 1));
+    let mut blocks: Vec<ColorBlock> = Vec::new();
+
     lookup_table.resize(hues.len() * (chromas.len() - 1) * (values.len() - 1), 0);
     let index = |h: usize, c: usize, v: usize| -> Option<usize> {
         if h > hues.len() {
@@ -234,6 +249,22 @@ fn validate_blocks(
                     }
                 }
             }
+
+            blocks.push(ColorBlock {
+                color_id: color_id,
+                hues: Range {
+                    start: hue_begin_index,
+                    end: hue_end_index,
+                },
+                chromas: Range {
+                    start: chroma_begin_index,
+                    end: chroma_end_index,
+                },
+                values: Range {
+                    start: value_begin_index,
+                    end: value_end_index,
+                },
+            })
         }
     }
 
@@ -253,6 +284,188 @@ fn validate_blocks(
             }
         }
     }
+
+    return blocks;
+}
+
+fn deinfinite(x: String) -> String {
+    if x == "INF" {
+        "9999".to_string()
+    } else {
+        x
+    }
+}
+
+fn linebreak(x: String) -> String {
+    return x.replace(" ", "\\n");
+}
+
+fn generate_gnuplot(
+    blocks: &Vec<ColorBlock>,
+    hues: &Vec<String>,
+    chromas: &Vec<String>,
+    values: &Vec<String>,
+    names: &HashMap<u32, ColorName>,
+) {
+    for h in 0..hues.len() {
+        let hue_blocks = blocks.iter().filter(|x| h == x.hues.start);
+
+        let basename = format!(
+            "doc/page{}-{}_hues_{}-{}",
+            16 + (h / 2),
+            h % 2,
+            hues[h],
+            hues[(h + 1) % hues.len()]
+        );
+        let mut file = File::create(format!("{}.gnu", basename)).unwrap();
+
+        writeln!(&mut file, "set encoding utf8").unwrap();
+        writeln!(&mut file, "set xrange [ 0.0 : 16.9 ]").unwrap();
+        writeln!(&mut file, "set yrange [ 0.0 : 10.4 ]").unwrap();
+        writeln!(&mut file, "set grid xtics ytics").unwrap();
+        writeln!(&mut file, "unset key").unwrap();
+        writeln!(&mut file, "set border 3").unwrap();
+        writeln!(&mut file, "set xlabel \"Munsell Chroma\"").unwrap();
+        writeln!(&mut file, "set ylabel \"Munsell Value\"").unwrap();
+        writeln!(
+            &mut file,
+            "set title \"{}-{}\" offset graph 0.45,0",
+            hues[h],
+            hues[(h + 1) % hues.len()]
+        )
+        .unwrap();
+
+        writeln!(&mut file, "set style fill empty").unwrap();
+        writeln!(&mut file, "set style line 1 default").unwrap();
+
+        let mut has_0p7 = false;
+        let mut has_1p2 = false;
+
+        for (id, block) in hue_blocks.enumerate() {
+            let x1 = chromas[block.chromas.start].clone();
+            let x2 = deinfinite(chromas[block.chromas.end].clone());
+            let y1 = values[block.values.start].clone();
+            let y2 = deinfinite(values[block.values.end].clone());
+
+            let x1f: f32 = x1.parse().unwrap();
+            let x2f: f32 = x2.parse::<f32>().unwrap().min(17.0);
+            let y1f: f32 = y1.parse().unwrap();
+            let y2f: f32 = y2.parse::<f32>().unwrap().min(10.5);
+
+            if x1 == "0.7" || x2 == "0.7" {
+                has_0p7 = true;
+            }
+
+            if x1 == "1.2" || x2 == "1.2" {
+                has_1p2 = true;
+            }
+
+            writeln!(
+                &mut file,
+                "set object {} polygon from {},{} to {},{} to {},{} to {},{} to {},{} default",
+                id + 1,
+                x1,
+                y1,
+                x1,
+                y2,
+                x2,
+                y2,
+                x2,
+                y1,
+                x1,
+                y1
+            )
+            .unwrap();
+
+            let label_x = (x1f + x2f) / 2.0;
+            let label_y = (y1f + y2f) / 2.0;
+
+            let is_vertical = (y2f - y1f) * 2.0 > (x2f - x1f);
+            let rotate = if is_vertical {
+                "rotate by 90"
+            } else {
+                "norotate"
+            };
+            let orig_name = names[&block.color_id].name.clone();
+
+            // gnuplot doesn't actually know about the bounding boxes in which we want to
+            // put these labels, which means it also doesn't know how to word-wrap them.
+            // Ideally I'd be able to compute this given the font and the string, but that's
+            // difficult, so just come up with a cheap-hopefully-good-enough-for-this-data
+            // approach.
+            let linebreak_thresh = (orig_name.len() as f32) * 0.2 + 0.5;
+            let should_linebreak = !is_vertical && (x2f - x1f) < linebreak_thresh;
+            let label_name = if should_linebreak {
+                linebreak(orig_name)
+            } else {
+                orig_name
+            };
+
+            // should this be done with the "labels" plot style instead?
+            writeln!(
+                &mut file,
+                "set label {} \"{{/:Bold {}}}: {}\" at first {},{} center {}",
+                id + 1,
+                block.color_id,
+                label_name,
+                label_x,
+                label_y,
+                rotate
+            )
+            .unwrap();
+        }
+
+        writeln!(
+            &mut file,
+            "set xtics border nomirror out scale 2.0 font 'Verdana,8'"
+        )
+        .unwrap();
+        writeln!(&mut file, "set xtics 0, 2.0").unwrap();
+        writeln!(&mut file, "set xtics add (1.0)").unwrap();
+        if has_0p7 {
+            writeln!(&mut file, "set xtics add (\"0.7\" 0.7 1)").unwrap();
+            writeln!(
+                &mut file,
+                "set label 100 \"0.7\" at first 0.65,-0.25 center font \"Verdana,6\""
+            )
+            .unwrap();
+        }
+        if has_1p2 {
+            writeln!(&mut file, "set xtics add (\"1.2\" 1.2 1)").unwrap();
+            writeln!(
+                &mut file,
+                "set label 101 \"1.2\" at first 1.25,-0.25 center font \"Verdana,6\""
+            )
+            .unwrap();
+        }
+
+        writeln!(&mut file, "set mxtics 2").unwrap();
+        writeln!(
+            &mut file,
+            "set ytics border nomirror out scale 2.0 font 'Verdana,8'"
+        )
+        .unwrap();
+        writeln!(&mut file, "set ytics 0, 1.0").unwrap();
+        writeln!(&mut file, "set mytics 2").unwrap();
+
+        writeln!(
+            &mut file,
+            "set terminal pngcairo size 600,800 enhanced font 'Verdana,7'"
+        )
+        .unwrap();
+        writeln!(&mut file, "set output '{}.png'", basename).unwrap();
+
+        // we need to plot _something_; can't just have polygons
+        writeln!(&mut file, "plot x+9999").unwrap();
+
+        // close and flush the file
+        drop(file);
+
+        Command::new("gnuplot")
+            .arg(format!("{}.gnu", basename))
+            .status()
+            .expect("failed to execute gnuplot");
+    }
 }
 
 fn main() {
@@ -268,11 +481,13 @@ fn main() {
         }
     };
 
-    validate_names(&doc);
+    let level3_names = validate_names(&doc);
 
     let hues = get_hues(&doc);
     let chromas = get_chromas(&doc);
     let values = get_values(&doc);
 
-    validate_blocks(&doc, &hues, &chromas, &values);
+    let blocks = validate_blocks(&doc, &hues, &chromas, &values);
+
+    generate_gnuplot(&blocks, &hues, &chromas, &values, &level3_names);
 }
